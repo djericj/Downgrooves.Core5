b208dc6e3167debc0b7ee1f573541f7465f1d3f2
Eric Rydberg <eric.rydberg@outlook.com>
2021-07-11 00:38:27 -0500

    Added Add methods to ITunesService

--- Downgrooves.Service/ITunesService.cs
+++ Downgrooves.Service/ITunesService.cs
@@ -1,7 +1,9 @@
 ﻿using Downgrooves.Domain;
 using Downgrooves.Persistence.Interfaces;
 using Downgrooves.Service.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace Downgrooves.Service
 {
@@ -14,9 +16,31 @@ namespace Downgrooves.Service
             _unitOfWork = unitOfWork;
         }
 
+        public void Add(ITunesTrack track)
+        {
+            _unitOfWork.ITunesTracks.Add(track);
+            _unitOfWork.Complete();
+        }
+
+        public void AddRange(IEnumerable<ITunesTrack> tracks)
+        {
+            _unitOfWork.ITunesTracks.AddRange(tracks);
+            _unitOfWork.Complete();
+        }
+
+        public IEnumerable<ITunesTrack> Find(Expression<Func<ITunesTrack, bool>> predicate)
+        {
+            return _unitOfWork.ITunesTracks.Find(predicate);
+        }
+
         public IEnumerable<ITunesTrack> GetTracks()
         {
             return _unitOfWork.ITunesTracks.GetAll();
+        }
+
+        public void Remove(ITunesTrack track)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Downgrooves.Service/Interfaces/IITunesService.cs
+++ Downgrooves.Service/Interfaces/IITunesService.cs
@@ -1,10 +1,20 @@
 ﻿using Downgrooves.Domain;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace Downgrooves.Service.Interfaces
 {
     public interface IITunesService
     {
         IEnumerable<ITunesTrack> GetTracks();
+
+        void Add(ITunesTrack track);
+
+        void AddRange(IEnumerable<ITunesTrack> tracks);
+
+        IEnumerable<ITunesTrack> Find(Expression<Func<ITunesTrack, bool>> predicate);
+
+        void Remove(ITunesTrack track);
     }
 }
